6583f2c1019e101aa0f8d933b3b515ff7983cd53
EDAbase <edabase@edabase.com>
2025-01-31 15:30:02 +0100

    Align name of output path in batch file

--- LivingDoc.bat
+++ LivingDoc.bat
@@ -2,6 +2,6 @@ echo off
 
 cd .\Expressium.Coffeeshop.Web.API.Tests\bin\Debug\net8.0
 
-Expressium.TestExecutionReport.exe .\TestExecution.json .\TestReport
+Expressium.TestExecutionReport.exe .\TestExecution.json .\TestExecutionReport
 
-start .\TestReport\LivingDoc.html
+start .\TestExecutionReport\LivingDoc.html
