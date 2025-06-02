7705af487b82e81f64ac5875b9771f5c48cce215
EDAbase <edabase@edabase.com>
2025-06-02 14:08:14 +0200

    add more cucumber message parser tests

--- Expressium.CucumberMessages.Tests/CucumberConvertorTests.cs
+++ Expressium.CucumberMessages.Tests/CucumberConvertorTests.cs
@@ -16,6 +16,7 @@ namespace Expressium.CucumberMessages.Tests
 
             Assert.That(livingDocProject.GetNumberOfFeatures(), Is.EqualTo(1));
             Assert.That(livingDocProject.GetNumberOfScenarios(), Is.EqualTo(2));
+            Assert.That(livingDocProject.GetNumberOfSteps(), Is.EqualTo(6));
         }
 
         [Test]
@@ -29,6 +30,49 @@ namespace Expressium.CucumberMessages.Tests
 
             Assert.That(livingDocProject.GetNumberOfFeatures(), Is.EqualTo(1));
             Assert.That(livingDocProject.GetNumberOfScenarios(), Is.EqualTo(3));
+            Assert.That(livingDocProject.GetNumberOfSteps(), Is.EqualTo(12));
         }
+
+        [Test]
+        public void Converting_Data_Table_Feature()
+        {
+            var inputFilePath = Path.Combine(Directory.GetCurrentDirectory(), "Samples", "data-tables.feature.ndjson");
+            var outputFilePath = Path.Combine(Directory.GetCurrentDirectory(), "Samples", "data-tables.feature.json");
+
+            var livingDocProject = CucumberConvertor.ConvertToLivingDoc(inputFilePath);
+            LivingDocUtilities.SerializeAsJson(outputFilePath, livingDocProject);
+
+            Assert.That(livingDocProject.GetNumberOfFeatures(), Is.EqualTo(1));
+            Assert.That(livingDocProject.GetNumberOfScenarios(), Is.EqualTo(1));
+            Assert.That(livingDocProject.GetNumberOfSteps(), Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Converting_Minimal_Feature()
+        {
+            var inputFilePath = Path.Combine(Directory.GetCurrentDirectory(), "Samples", "minimal.feature.ndjson");
+            var outputFilePath = Path.Combine(Directory.GetCurrentDirectory(), "Samples", "minimal.feature.json");
+
+            var livingDocProject = CucumberConvertor.ConvertToLivingDoc(inputFilePath);
+            LivingDocUtilities.SerializeAsJson(outputFilePath, livingDocProject);
+
+            Assert.That(livingDocProject.GetNumberOfFeatures(), Is.EqualTo(1));
+            Assert.That(livingDocProject.GetNumberOfScenarios(), Is.EqualTo(1));
+            Assert.That(livingDocProject.GetNumberOfSteps(), Is.EqualTo(1));
+        }
+
+        //[Test]
+        //public void Converting_Skipped_Feature()
+        //{
+        //    var inputFilePath = Path.Combine(Directory.GetCurrentDirectory(), "Samples", "skipped.feature.ndjson");
+        //    var outputFilePath = Path.Combine(Directory.GetCurrentDirectory(), "Samples", "skipped.feature.json");
+
+        //    var livingDocProject = CucumberConvertor.ConvertToLivingDoc(inputFilePath);
+        //    LivingDocUtilities.SerializeAsJson(outputFilePath, livingDocProject);
+
+        //    Assert.That(livingDocProject.GetNumberOfFeatures(), Is.EqualTo(1));
+        //    Assert.That(livingDocProject.GetNumberOfScenarios(), Is.EqualTo(1));
+        //    Assert.That(livingDocProject.GetNumberOfSteps(), Is.EqualTo(1));
+        //}
     }
 }
