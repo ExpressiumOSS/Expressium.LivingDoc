aeaa8e486fc17e2907b2fca3887992e9124813bd
EDAbase <edabase@edabase.com>
2025-07-21 13:25:57 +0200

    Improve scenario Edito page

--- Expressium.LivingDoc/Generators/LivingDocBodyGenerator.cs
+++ Expressium.LivingDoc/Generators/LivingDocBodyGenerator.cs
@@ -70,7 +70,7 @@ namespace Expressium.LivingDoc.Generators
 
             if (includeEditor)
             {
-                listOfLines.Add("<a class='navigation-link' title='Gherkin Script Editor' href='#' onclick=\"loadEditor('editor'); filterStepDefinitions();\">Editor</a>");
+                listOfLines.Add("<a class='navigation-link' title='Gherkin Scenario Editor' href='#' onclick=\"loadEditor('editor'); filterStepDefinitions();\">Editor</a>");
                 listOfLines.Add("<span>|</span>");
             }
 
--- Expressium.LivingDoc/Generators/LivingDocDataEditorGenerator.cs
+++ Expressium.LivingDoc/Generators/LivingDocDataEditorGenerator.cs
@@ -22,28 +22,32 @@ namespace Expressium.LivingDoc.Generators
             listOfLines.Add($"<div class='data-item' id='editor'>");
 
             listOfLines.Add("<div class='section'>");
-            listOfLines.Add("<span class='page-name'>Gherkin Script Editor</span>");
+            listOfLines.Add("<span class='project-name'>Gherkin Scenario Editor</span>");
             listOfLines.Add("</div>");
 
-            //listOfLines.Add("<!-- Editor Actions Section -->");
-            //listOfLines.Add("<div class='section layout-row'>");
-            //listOfLines.Add("<div class='layout-column align-right'>");
-            //listOfLines.Add("<button title='Preview Scenario Script' class='toolbox-options' onclick='editorPreview()'>Preview</button>");
-            //listOfLines.Add("<button title='Copy Scenario Script' class='toolbox-options' onclick='editorCopy()'>Copy</button>");
-            //listOfLines.Add("<button title='Format Scenario Script' class='toolbox-options' onclick='editorFormat()'>Format</button>");
-            //listOfLines.Add("<button title='Clear Scenario Script' class='toolbox-options' onclick='editorClear()'>Clear</button>");
-            //listOfLines.Add("<button title='Download Scenario Script' class='toolbox-options' onclick='editorDownload()'>Download</button>");
-            //listOfLines.Add("</div>");
-            //listOfLines.Add("</div>");
+            listOfLines.Add("<!-- Editor Actions Section -->");
+            listOfLines.Add("<div class='section layout-row'>");
+
+            listOfLines.Add("<div class='layout-column align-left'>");
+            listOfLines.Add("<span class='page-name'>Scenario</span>");
+            listOfLines.Add("</div>");
+
+            listOfLines.Add("<div class='layout-column align-right'>");
+            listOfLines.Add("<button title='Preview Scenario Script' class='toolbox-options' onclick='editorPreview()'>Preview</button>");
+            listOfLines.Add("<button title='Copy Scenario Script' class='toolbox-options' onclick='editorCopy()'>Copy</button>");
+            listOfLines.Add("<button title='Format Scenario Script' class='toolbox-options' onclick='editorFormat()'>Format</button>");
+            listOfLines.Add("<button title='Clear Scenario Script' class='toolbox-options' onclick='editorClear()'>Clear</button>");
+            listOfLines.Add("<button title='Download Scenario Script' class='toolbox-options' onclick='editorDownload()'>Download</button>");
+            listOfLines.Add("</div>");
+
+            listOfLines.Add("</div>");
 
             listOfLines.Add("<div class='section'>");
-            listOfLines.Add("<span class='scenario-keyword'>Scenario:</span>");
-            listOfLines.Add("<br />");
             listOfLines.Add("<textarea class='filter' id='script' rows='7'></textarea>");
             listOfLines.Add("</div>");
 
             listOfLines.Add("<div class='section'>");
-            listOfLines.Add("<span class='scenario-keyword'>Step Definitions:</span>");
+            listOfLines.Add("<span class='page-name'>Step Definitions</span>");
             listOfLines.Add("</div>");
 
             listOfLines.Add("<div class='section'>");
