6c95b469c2fed5cd09569607584af782a103cf40
EDAbase <edabase@edabase.com>
2025-07-28 15:55:00 +0200

    Add LivingDocDataAnalyticsGeneratorTests

---
+++ Expressium.LivingDoc.Tests/Generators/LivingDocDataAnalyticsGeneratorTests.cs
@@ -0,0 +1,31 @@
+using Expressium.LivingDoc.Generators;
+
+namespace Expressium.LivingDoc.Tests.Generators
+{
+    [TestFixture]
+    public class LivingDocDataAnalyticsGeneratorTests
+    {
+        private LivingDocDataAnalyticsGenerator dataAnalyticsGenerator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            dataAnalyticsGenerator = new LivingDocDataAnalyticsGenerator();
+        }
+
+        [TestCase(0, 10, ExpectedResult = 0)]
+        [TestCase(5, 10, ExpectedResult = 50)]
+        [TestCase(1, 100, ExpectedResult = 1)]
+        [TestCase(99, 100, ExpectedResult = 99)]
+        [TestCase(1, 200, ExpectedResult = 1)]
+        [TestCase(2, 3, ExpectedResult = 67)]
+        [TestCase(1, 3, ExpectedResult = 33)]
+        [TestCase(3, 3, ExpectedResult = 100)]
+        [TestCase(0, 1, ExpectedResult = 0)]
+        [TestCase(1, 1, ExpectedResult = 100)]
+        public int CalculatePercentage_ReturnsExpectedResult(int numberOfStatuses, int numberOfTests)
+        {
+            return dataAnalyticsGenerator.CalculatePercentage(numberOfStatuses, numberOfTests);
+        }
+    }
+}
