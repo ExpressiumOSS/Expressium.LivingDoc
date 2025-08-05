1b364ad98f6460ca4851279919095887a6727b92
EDAbase <edabase@edabase.com>
2025-08-05 08:42:21 +0200

    Update filterDataFolders script function

--- Expressium.LivingDoc/Generators/LivingDocDataViewsGeneratorEx.cs
+++ Expressium.LivingDoc/Generators/LivingDocDataViewsGeneratorEx.cs
@@ -109,7 +109,7 @@ namespace Expressium.LivingDoc.Generators
 
             listOfLines.Add($"<td width='16px;' style='font-size: 0.940em; padding: 2px; text-align: center;'>📂</td>");
             listOfLines.Add($"<td class='gridline' colspan='{numberOfColumns - indent}'>");
-            listOfLines.Add($"<span><b>{folder.Split("\\").Last()}</b></span>");
+            listOfLines.Add($"<span><b>{folder?.Split("\\").Last() ?? ""}</b></span>");
             listOfLines.Add($"</td>");
             listOfLines.Add($"<td class='gridline' align='right'></td>");
 
