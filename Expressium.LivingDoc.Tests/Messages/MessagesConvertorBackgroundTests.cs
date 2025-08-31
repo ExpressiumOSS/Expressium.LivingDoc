d307b1d5c226e4425d713f4206d67907fab968bb
EDAbase <edabase@edabase.com>
2025-08-31 11:48:35 +0200

    Add addition tests with failing backgrounds steps

--- Expressium.LivingDoc.Tests/Messages/MessagesConvertorBackgroundTests.cs
+++ Expressium.LivingDoc.Tests/Messages/MessagesConvertorBackgroundTests.cs
@@ -1,4 +1,5 @@
 ﻿using Expressium.LivingDoc.Messages;
+using Expressium.LivingDoc.Models;
 using System.IO;
 
 namespace Expressium.LivingDoc.UnitTests.Messages
@@ -22,6 +23,30 @@ namespace Expressium.LivingDoc.UnitTests.Messages
 
             Assert.That(feature.Background.Steps[0].Name, Is.EqualTo("I have $500 in my checking account"));
             Assert.That(feature.Scenarios[0].Examples[0].Steps[0].Name, Is.EqualTo("I have $500 in my checking account"));
+        }
+
+        [Test]
+        public void Converting_Feature_Backgrounds()
+        {
+            var inputFilePath = Path.Combine(Directory.GetCurrentDirectory(), "Samples", "backgrounds.features.ndjson");
+
+            var messagesConvertor = new MessagesConvertor();
+            var livingDocProject = messagesConvertor.ConvertToLivingDoc(inputFilePath);
+
+            Assert.That(livingDocProject.GetNumberOfFeatures(), Is.EqualTo(1));
+            Assert.That(livingDocProject.GetNumberOfScenarios(), Is.EqualTo(2));
+            Assert.That(livingDocProject.GetNumberOfExamples(), Is.EqualTo(2));
+            Assert.That(livingDocProject.GetNumberOfSteps(), Is.EqualTo(4));
+
+            var feature = livingDocProject.Features[0];
+
+            Assert.That(feature.Background.Steps[0].Name, Is.EqualTo("I have an initial step running"));
+
+            Assert.That(feature.Scenarios[0].Examples[0].Steps[0].Name, Is.EqualTo("I have an initial step running"));
+            Assert.That(feature.Scenarios[0].Examples[0].Steps[0].Status, Is.EqualTo(LivingDocStatuses.Passed.ToString()));
+
+            Assert.That(feature.Scenarios[1].Examples[0].Steps[0].Name, Is.EqualTo("I have an initial step running"));
+            Assert.That(feature.Scenarios[1].Examples[0].Steps[0].Status, Is.EqualTo(LivingDocStatuses.Failed.ToString()));
         }
     }
 }
---
+++ Expressium.LivingDoc.Tests/Samples/backgrounds.feature
@@ -0,0 +1,26 @@
+﻿Feature: Backgrounds
+
+Background:
+	Given I have an initial step running
+
+@passed
+Scenario: Scenario without Examples and Passing Background Step
+	Given I have a Passed step running
+
+@failed
+Scenario: Scenario without Examples and Failing Background Step
+	Given I have a Passed step running
+
+#@passed
+#Scenario: Scenario with Examples and Passing Background Step
+#	Given I have a <status> step running
+#Examples:
+#	| status | Level |
+#	| Passed | 1     |
+#
+#@failed
+#Scenario: Scenario with Examples and Failing Background Step
+#	Given I have a <status> step running
+#Examples:
+#	| status | Level |
+#	| Passed | 2     |
