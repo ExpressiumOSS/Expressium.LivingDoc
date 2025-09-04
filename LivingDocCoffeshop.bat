d55f02751240a14498840130240f9b1eabc7ee52
EDAbase <edabase@edabase.com>
2025-09-04 12:04:34 +0200

    Remove Coffeeshop test project and API Project

--- LivingDocCoffeshop.bat
+++
@@ -1,9 +0,0 @@
-echo off
-
-cd .\Expressium.Coffeeshop.Web.API.Tests\bin\Debug\net8.0
-
-start .\ReqnRoll.html
-
-del .\LivingDoc.html
-Expressium.LivingDoc.exe -input .\ReqnRoll.ndjson --output .\LivingDoc.html --title "Expressium.Coffeeshop.Web.API.Tests"
-start .\LivingDoc.html
--- LivingDocPerago.bat
+++
@@ -1,8 +0,0 @@
-echo off
-
-cd .\Expressium.Coffeeshop.Web.API.Tests\bin\Debug\net8.0
-
-del .\LivingDocPerago.html
-Expressium.LivingDoc.exe --native c:\Temp\TestExecutionEx.json .\LivingDocPerago.html
-
-start .\LivingDocPerago.html
