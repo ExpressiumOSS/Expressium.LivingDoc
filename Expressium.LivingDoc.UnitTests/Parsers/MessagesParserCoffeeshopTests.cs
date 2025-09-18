ca84779bf71930539d6d4f0e8f325ffee366fff6
EDAbase <edabase@edabase.com>
2025-09-18 22:23:53 +0200

    Add UnitTest for Coffeeshop examples duration and attachments

---
+++ Expressium.LivingDoc.UnitTests/Parsers/MessagesParserCoffeeshopTests.cs
@@ -0,0 +1,30 @@
+﻿using Expressium.LivingDoc.Parsers;
+using System.IO;
+
+namespace Expressium.LivingDoc.UnitTests.Parsers
+{
+    internal class MessagesParserCoffeeshopTests
+    {
+        [Test]
+        public void Converting_Scenario_Coffeeshop()
+        {
+            var inputFilePath = Path.Combine(Directory.GetCurrentDirectory(), "Samples", "coffeeshop.feature.ndjson");
+
+            var messagesParser = new MessagesParser();
+            var livingDocProject = messagesParser.ConvertToLivingDoc(inputFilePath);
+
+            var livingDocFeature = livingDocProject.Features[3];
+            Assert.That(livingDocFeature.Name, Is.EqualTo("Orders"));
+
+            var livingDocScenario = livingDocFeature.Scenarios[0];
+            Assert.That(livingDocScenario.Name, Is.EqualTo("Ordering Coffee Confirmation Notification"));
+            Assert.That(livingDocScenario.HasDataTable(), Is.True);
+
+            Assert.That(livingDocScenario.Examples[0].GetDuration(), Is.EqualTo("5s 473ms"));
+            Assert.That(livingDocScenario.Examples[0].Attachments[0], Does.Contain("SantaRitaCerradoMineiro $99"));
+
+            Assert.That(livingDocScenario.Examples[1].GetDuration(), Is.EqualTo("4s 739ms"));
+            Assert.That(livingDocScenario.Examples[1].Attachments[0], Does.Contain("LaSoledadAntioquia $77"));
+        }
+    }
+}
