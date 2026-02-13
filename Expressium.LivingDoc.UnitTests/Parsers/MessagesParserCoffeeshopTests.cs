52da495f34f10a7c2d30e6e29e6836080b5b850d
Expressium <edabase@edabase.com>
2026-02-13 13:16:34 +0100

    Update Coffeeshop NDJSON unittest exmaple file

--- Expressium.LivingDoc.UnitTests/Parsers/MessagesParserCoffeeshopTests.cs
+++ Expressium.LivingDoc.UnitTests/Parsers/MessagesParserCoffeeshopTests.cs
@@ -13,17 +13,17 @@ namespace Expressium.LivingDoc.UnitTests.Parsers
             var messagesParser = new MessagesParser();
             var livingDocProject = messagesParser.ConvertToLivingDoc(inputFilePath);
 
-            var livingDocFeature = livingDocProject.Features[2];
+            var livingDocFeature = livingDocProject.Features[3];
             Assert.That(livingDocFeature.Name, Is.EqualTo("Orders"));
 
             var livingDocScenario = livingDocFeature.Scenarios[0];
             Assert.That(livingDocScenario.Name, Is.EqualTo("Ordering Coffee Confirmation Notification"));
             Assert.That(livingDocScenario.HasDataTable(), Is.True);
 
-            Assert.That(livingDocScenario.Examples[0].GetDuration(), Is.EqualTo("4s 393ms"));
+            Assert.That(livingDocScenario.Examples[0].GetDuration(), Is.EqualTo("5s 428ms"));
             Assert.That(livingDocScenario.Examples[0].Attachments[0], Does.Contain("SantaRitaCerradoMineiro $99"));
 
-            Assert.That(livingDocScenario.Examples[1].GetDuration(), Is.EqualTo("5s 628ms"));
+            Assert.That(livingDocScenario.Examples[1].GetDuration(), Is.EqualTo("4s 449ms"));
             Assert.That(livingDocScenario.Examples[1].Attachments[0], Does.Contain("LaSoledadAntioquia $77"));
         }
     }
