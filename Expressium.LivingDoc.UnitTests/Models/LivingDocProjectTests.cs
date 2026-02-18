fceb587ab865f36e4a20842001359b437d7052f3
Expressium <edabase@edabase.com>
2026-02-18 16:49:55 +0100

    Additional UnitTests for MergeHistory in LivingDocProject

--- Expressium.LivingDoc.UnitTests/Models/LivingDocProjectTests.cs
+++ Expressium.LivingDoc.UnitTests/Models/LivingDocProjectTests.cs
@@ -140,5 +140,86 @@ namespace Expressium.LivingDoc.UnitTests.Models
 
             Assert.That(livingDocProject.GetDuration(), Is.EqualTo(result));
         }
+
+        [Test]
+        public void LivingDocProject_MergeHistory()
+        {
+            var livingDocProjectMaster = new LivingDocProject();
+
+            var livingDocProjectSlave = new LivingDocProject();
+            livingDocProjectSlave.Date = System.DateTime.UtcNow;
+
+            // Passed feature
+            var passedFeature = new LivingDocFeature { Name = "PassedFeature" };
+            var passedScenario = new LivingDocScenario();
+            var passedExample = new LivingDocExample();
+            passedExample.Steps.Add(new LivingDocStep { Status = LivingDocStatuses.Passed.ToString() });
+            passedScenario.Examples.Add(passedExample);
+            passedFeature.Scenarios.Add(passedScenario);
+            livingDocProjectSlave.Features.Add(passedFeature);
+
+            // Failed feature
+            var failedFeature = new LivingDocFeature { Name = "FailedFeature" };
+            var failedScenario = new LivingDocScenario();
+            var failedExample = new LivingDocExample();
+            failedExample.Steps.Add(new LivingDocStep { Status = LivingDocStatuses.Failed.ToString() });
+            failedScenario.Examples.Add(failedExample);
+            failedFeature.Scenarios.Add(failedScenario);
+            livingDocProjectSlave.Features.Add(failedFeature);
+
+            // Incomplete feature
+            var incompleteFeature = new LivingDocFeature { Name = "IncompleteFeature" };
+            var incompleteScenario = new LivingDocScenario();
+            var incompleteExample = new LivingDocExample();
+            incompleteExample.Steps.Add(new LivingDocStep { Status = LivingDocStatuses.Incomplete.ToString() });
+            incompleteScenario.Examples.Add(incompleteExample);
+            incompleteFeature.Scenarios.Add(incompleteScenario);
+            livingDocProjectSlave.Features.Add(incompleteFeature);
+
+            // Skipped feature
+            var skippedFeature = new LivingDocFeature { Name = "SkippedFeature" };
+            livingDocProjectSlave.Features.Add(skippedFeature);
+
+            livingDocProjectMaster.MergeHistory(livingDocProjectSlave, "http://history-url");
+            Assert.That(livingDocProjectMaster.Histories.Count, Is.EqualTo(1));
+            Assert.That(livingDocProjectMaster.Histories[0].Date, Is.EqualTo(livingDocProjectSlave.Date));
+            Assert.That(livingDocProjectMaster.Histories[0].Url, Is.EqualTo("http://history-url"));
+            Assert.That(livingDocProjectMaster.Histories[0].Features.Passed, Does.Contain("PassedFeature"));
+            Assert.That(livingDocProjectMaster.Histories[0].Features.Failed, Does.Contain("FailedFeature"));
+            Assert.That(livingDocProjectMaster.Histories[0].Features.Incomplete, Does.Contain("IncompleteFeature"));
+            Assert.That(livingDocProjectMaster.Histories[0].Features.Skipped, Does.Contain("SkippedFeature"));
+
+            // Calling MergeHistory again should omit duplicates...
+            livingDocProjectMaster.MergeHistory(livingDocProjectSlave, "http://history-url");
+            Assert.That(livingDocProjectMaster.Histories.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void LivingDocProject_GetMaximumNumberOfHistoryCounts()
+        {
+            var livingDocProject = new LivingDocProject();
+
+            var historyOne = new LivingDocHistory();
+            historyOne.Date = System.DateTime.UtcNow.AddDays(-1);
+            historyOne.Features.Passed.AddRange(new[] { "F1", "F2" });
+            historyOne.Features.Failed.Add("F3");
+            historyOne.Scenarios.Passed.AddRange(new[] { "S1", "S2", "S3" });
+            historyOne.Scenarios.Incomplete.Add("S4");
+            historyOne.Steps.Passed.AddRange(new[] { "Step1", "Step2", "Step3", "Step4" });
+
+            var historyTwo = new LivingDocHistory();
+            historyTwo.Date = System.DateTime.UtcNow;
+            historyTwo.Features.Passed.AddRange(new[] { "F4", "F5", "F6", "F7", "F8" });
+            historyTwo.Scenarios.Passed.AddRange(new[] { "S5", "S6" });
+            historyTwo.Steps.Passed.AddRange(new[] { "Step5", "Step6", "Step7", "Step8", "Step9", "Step10" });
+            historyTwo.Steps.Failed.Add("Step11");
+
+            livingDocProject.Histories.Add(historyOne);
+            livingDocProject.Histories.Add(historyTwo);
+
+            Assert.That(livingDocProject.GetMaximumNumberOfHistoryFeatures(), Is.EqualTo(5));
+            Assert.That(livingDocProject.GetMaximumNumberOfHistoryScenarios(), Is.EqualTo(4));
+            Assert.That(livingDocProject.GetMaximumNumberOfHistorySteps(), Is.EqualTo(7));
+        }
     }
 }
